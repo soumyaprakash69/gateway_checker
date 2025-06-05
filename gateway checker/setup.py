cb42ecbe32191f7e0816ee9e0b88f360e49c8c34
soumyaprakash69 <prakash90785490@gmail.com>
2025-06-05 18:56:26 +0530

    Add files via upload

---
+++ gateway checker/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gateway_checker",
+    version="1.0",
+    install_requires=[
+        "pyrogram>=2.0.0",
+        "tgcrypto>=1.2.0",
+        "requests>=2.25.0",
+    ],
+)
